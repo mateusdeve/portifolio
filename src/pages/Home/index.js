4e2ec38a35f4439928fde9d6d34addfce7278587
Mateus Pires <mateus.dev.ti@gmail.com>
2020-08-23 15:58:43 -0300

    criando secao de comentarios

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -11,11 +11,12 @@ import css from '../../assets/img/css.png'
 import js from '../../assets/img/js.png'
 import react from '../../assets/img/react.png'
 import xd from '../../assets/img/xd.png'
+import boneco from '../../assets/img/boneco.png'
 
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 const Home = () => {
     return (
@@ -53,7 +54,7 @@ const Home = () => {
                             <CardContent className="contentCard">
                                 <img src={perfil}  className="perfil" alt=""/>
                             </CardContent>
-                                    <p className="titulo">Mateus Pires</p>
+                                <p className="titulo">Mateus Pires</p>
                                 <div className="groupContact">
                                     <div style={{marginTop: 20}}>
                                         <img className="icon" src={github} alt=""/>
@@ -64,7 +65,6 @@ const Home = () => {
                                         <a  href="https://linkedin.com/in/mateussilveirapires">linkedin.com/mateussilveirapires</a>
                                     </div>
                                 </div>
-                            
                         </Card>
                     </div>
 
@@ -74,36 +74,87 @@ const Home = () => {
                 <div className="tec">
                     <h1>Tecnologias</h1>
                 </div>
-                <Grid item xs={6} className="inline gridWidth">
-                    <div className="back">
-                        <h1>BACK-END</h1>
-                        <div className="groupBack">
-                            <p>~$ Node.JS</p>
-                            <div>
-                                <p> Express.js</p>
-                                <p> MongoDB</p>
-                                <p> MySQL</p>
-                                <p> PostegreSQL |</p>
+                <Grid container>
+                    <Grid item xs={6}>
+                        <div className="back">
+                            <h1>BACK-END</h1>
+                            <div className="groupBack">
+                                <p>~$ Node.JS</p>
+                                <div>
+                                    <p> Express.js</p>
+                                    <p> MongoDB</p>
+                                    <p> MySQL</p>
+                                    <p> PostegreSQL |</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </Grid>
-                <Grid item xs={6} className="inline gridWidth">
-                    <div className="front">
-                        <h1>FRONT-END</h1>
-                        <div className="groupFront">
-                            <Grid item xs={3}>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <div className="front">
+                            <h1>FRONT-END</h1>
+                            <div className="groupFront">
                                 <img src={html} alt=""/>
-                            </Grid>
-                            <Grid item xs={3}>
-                                <img src={css} alt=""/>
-                            </Grid>
-                            <Grid item xs={3}>
-                                <img src={js} alt=""/>
-                            </Grid>
+                                <img className="spacoee" src={css} alt=""/>
+                                <img className="spacoee" src={js} alt=""/>
+                            </div>
+                                <div className="groupFront">
+                                    <img className="width" src={react} alt=""/>
+                                    <img className="spacoee width" src={xd} alt=""/>
+                                </div>
                         </div>
-                    </div>
+                    </Grid>
                 </Grid>
+            </div>
+            <div className="secao4">
+                <div className="comen">
+                    <h1>Comentarios</h1>
+                </div>
+                <Grid container>
+                    <Grid item xs={6}>
+                    <form style={{marginTop:90}}>
+                        <Grid container>
+                            <Grid item xs={2}></Grid>
+                        <Grid item xs={4}>
+                            <TextField id="standard-basic" label="Nome" />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField id="standard-basic" label="Titulo" />
+                        </Grid>
+                        <Grid item xs={2}></Grid>
+                        <Grid item xs={10} style={{marginTop: 50}}>
+                            <TextField
+                                id="standard-multiline-static"
+                                label="Comentario"
+                                multiline
+                                rows={4}
+                                className="textArea"
+                                />
+                        </Grid>
+
+                        <Grid item xs={2}></Grid>
+                        <Grid item xs={10}>
+                            <Button className="button" style={{marginTop:50}} variant="contained">SOBRE</Button>
+                        </Grid>
+                        </Grid>
+                    </form>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Grid container>
+
+                        <Grid item xs={3}></Grid>
+                        <Grid item xs={7}>
+                            <div>
+                                <img src={boneco} className="boneco" alt=""/>
+                            </div>
+                        </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </div>
+            <div className="secao5">
+            <div className="comen">
+                    <h1>Ultimos Comentarios</h1>
+                </div>
             </div>
         </>
     )
